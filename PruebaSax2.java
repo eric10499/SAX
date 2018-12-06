cd25166b4c081694d61141d445d03cd6415f7b91
eric10499 <34514368+eric10499@users.noreply.github.com>
2018-12-06 20:20:12 +0100

    Create PruebaSax2.java

---
+++ PruebaSax2.java
@@ -0,0 +1,36 @@
+package FicherosAleatorios;
+import java.io.*;
+import java.util.Scanner;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+public class PruebaSax2 {   
+
+	public static void main (String [] args)           
+			throws FileNotFoundException, IOException, SAXException, ParserConfigurationException {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Indica el nombre del artista: ");
+		String artista = scanner.nextLine();
+		/* A continuación se crea objeto procesador XML - XMLReader -. 
+		 * Durante la creación de este objeto se puede producir una excepción SAXException. */
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		SAXParser parser = factory.newSAXParser();
+		XMLReader reader = parser.getXMLReader();
+		
+		/* A continuación, mediante setContentHandler establecemos que la clase que gestiona los eventos provocados por la lectura del XML será GestionContenido */ 
+		GestionContenido2 gestor = new GestionContenido2(artista); 
+		reader.setContentHandler(gestor);
+		/* Por último, se define el fichero que se va leer mediante InputSource y se procesa el documento XML mediante el método parse() de XMLReader */    
+		InputSource fileXML = new InputSource ("C:\\\\Users\\\\ERIC\\\\Desktop\\\\discoteca.xml");
+		reader.parse(fileXML); 
+	} 
+}
