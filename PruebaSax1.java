7e2039062c17b6411ed81355e22a1c382cb3d135
eric10499 <34514368+eric10499@users.noreply.github.com>
2018-12-16 12:27:12 +0100

    Update PruebaSax1.java

--- PruebaSax1.java
+++ PruebaSax1.java
@@ -1,3 +1,29 @@
+/*
+ * PruebaSax1.java
+ * 
+ * Copyright 2018 ERIC <ERIC@DESKTOP-5T1N085>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * Crea un programa que lea el archivo “discoteca.xml” (adjunto en el moodle) y determine el número de discos listados en el archivo. AYUDA: contabilizando el número de títulos.
+ * 
+ * Fet per Eric Visier Sánchez
+ * 
+ */
+
 package FicherosAleatorios;
 import java.io.*;
 import java.util.Scanner;
@@ -21,7 +47,9 @@ public class PruebaSax1 {
 		System.out.println("Indica el nombre del artista: ");
 		String artista = scanner.nextLine();
 		/* A continuación se crea objeto procesador XML - XMLReader -. 
-		 * Durante la creación de este objeto se puede producir una excepción SAXException. */
+		 * Durante la creación de este objeto se puede producir una excepción SAXException. 
+		 * XMLReaderFactory deprecated desde la version 9 Java*/
+		
 		SAXParserFactory factory = SAXParserFactory.newInstance();
 		SAXParser parser = factory.newSAXParser();
 		XMLReader reader = parser.getXMLReader();
