749fca5e38620c3e632649966bc96399a225bee5
eric10499 <34514368+eric10499@users.noreply.github.com>
2018-12-16 12:28:30 +0100

    Update PruebaSax2.java

--- PruebaSax2.java
+++ PruebaSax2.java
@@ -1,3 +1,29 @@
+/*
+ * PruebaSax2.java
+ * 
+ * Copyright 2018 ERIC <ERIC@DESKTOP-5T1N085>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * Crea un programa que devuelva el número de discos registrados en “discoteca.xml” de un determinado autor que le pasamos por consola. Si el autor carece de discos en el archivo, el programa devolverá un mensaje del estilo: “El autor <xxxxxx> no aparece en el archivo.
+ * 
+ * Fet per Eric Visier Sánchez
+ * 
+ */
+
 package FicherosAleatorios;
 import java.io.*;
 import java.util.Scanner;
